0db421307ea45bc4507f5490dd83d54116835f04
kiattisak jomram <t.kiattisak.j@gmail.com>
2025-11-30 08:27:27 +0700

    feat: implement user deletion functionality across API, use case, and repository layers.

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -11,9 +11,11 @@ type User struct {
 type UserRepository interface {
 	Create(ctx context.Context, user *User) error
 	GetByID(ctx context.Context, id int64) (*User, error)
+	Delete(ctx context.Context, id int64) error
 }
 
 type UserUsecase interface {
 	Register(ctx context.Context, user *User) error
 	GetUser(ctx context.Context, id int64) (*User, error)
+	DeleteUser(ctx context.Context, id int64) error
 }
